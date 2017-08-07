d8d372e4365c44fa6376abb2672b16edaebaeef6
apiconz <picondev@gmail.com>
2017-08-07 12:26:10 -0500

    Initial commit

---
+++ app/src/main/java/pe/devpicon/android/myrxjava2application/MainActivity.java
@@ -0,0 +1,13 @@
+package pe.devpicon.android.myrxjava2application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
